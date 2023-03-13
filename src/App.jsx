da648f80bf4f55d147ad5049e30cfed5881e157c
fedderica <fedderica.bartoli@gmail.com>
2023-03-13 17:08:24 -0300

    Po

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export function App() {
+    return <div>
+        <header>
+            <h1>Movies</h1>
+        </header>
+        <main>
+            <ul>
+                <li> Movie 1</li>
+                <li> Movie 2</li>
+                <li> Movie 3 </li>
+            </ul>
+        </main>
+    </div>;
+};
+
+//Si yo selecciono F2 cambio el nombre de los componentes en todos sus espacios
+//React puede crear interfaces de usuario de forma reutilizable
+//este es mi componente
--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { App } from "./App";
 
-const contenido = (
-<div>
-  <h1>Titulo</h1>
-  <div>Hola desde react</div>
-  </div>
-);
-
-function Componente({ título, children}) {
-
-  return ( 
-    <div>
-      <h1>{titulo}</h1>
-      <div>{children}</div>
-    </div>
-  );
-}
 
    ReactDOM.render(
-   <Componente titulo="Titulo 1"> Hola desde React</Componente>,
+    <App/>,
     document.getElementById('root')
     );
 
-    
+
 // Aquí le envie propiedades al componente- forma básica y sencilla en React
