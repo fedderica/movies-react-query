9c42dd588ceaf7bae1e829cc0ae916a31c81f3bb
fedderica <fedderica.bartoli@gmail.com>
2023-03-14 21:44:39 -0300

    ingresando.images.stylos

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,10 +1,10 @@
-import styles from "./MoviesCard.module.css";
+import styles from "./MovieCard.module.css";
 export function MovieCard({ movie }) {
     console.log(styles);
      const imageUrl = "http://image.tmdb.org/t/p/w300" + movie.poster_
     return (
           <li className = {styles.movieCard}>
-       <img src={imageUrl} alt={movie.title} />
+       <img className={styles.movieImage} alt={movie.title} />
      <div>{movie.title}</div>
        </li>
     );
