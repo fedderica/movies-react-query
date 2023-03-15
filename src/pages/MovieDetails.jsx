afd29649592e4eef51fea67008fbb95fa6d9a082
fedderica <fedderica.bartoli@gmail.com>
2023-03-15 16:28:47 -0300

    Configuración API-DESCRIPCION

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,3 +1,15 @@
+import movie from "./movie.json";
+import styles from "./MovieDetails.module.css";
+
 export function MovieDetails(){
-    return <div>Details</div>;
+    const imageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+    return (
+    <div className={styles.detailsContainer}>
+    <img src={imageUrl} alt={movie.title} />
+    <div>
+        <p>Title: {movie.title} </p> 
+        <p> Description: {movie.overview} </p>
+        </div>
+        </div>
+    );
 }
