a051d391eb5bd695803a159b5063edc3b53bf6fd
fedderica <fedderica.bartoli@gmail.com>
2023-03-15 18:08:24 -0300

    agrega-stylos-api-control-y-descripcion-peliculas

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -5,10 +5,22 @@ export function MovieDetails(){
     const imageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
     return (
     <div className={styles.detailsContainer}>
-    <img src={imageUrl} alt={movie.title} />
-    <div>
-        <p>Title: {movie.title} </p> 
-        <p> Description: {movie.overview} </p>
+    <img
+     className={styles.col + " " + styles.movieImage} 
+    src={imageUrl}    
+    alt={movie.title}
+     />
+    <div className={`${styles.col} ${styles.movieDetails}`}>
+        <p>
+           <strong> Title:</strong> {movie.title}
+         </p> 
+        <p>
+            <strong>Genres:</strong>{" "}
+            {movie.genres.map((genre) => genre.name).join(", ")}
+            </p>
+            <p>
+                 <strong> Description: </strong> {movie.overview} 
+                 </p>
         </div>
         </div>
     );
