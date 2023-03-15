9c602ddd36e113a919be7028b1f7460e7215b270
fedderica <fedderica.bartoli@gmail.com>
2023-03-15 15:03:25 -0300

    Modificacióndatos+css+api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { MoviesGrid } from "./components/MoviesGrid";
+//import { MoviesGrid } from "./components/MoviesGrid";
 //import movies from "../movies.json";
 import styles from  "./App.module.css";
 import { BrowserRouter as Router, Routes,  Route, Link } from "react-router-dom";
@@ -6,29 +6,30 @@ import { MovieDetails } from "./pages/MovieDetails";
 import { LandingPage } from "./pages/LandingPage"
 
 
-export function App() {
-    return (
 
-    <Router>
-        <header>
-            <Link to="/">
-                <h1 className={styles.title}>Movies</h1>
-                </Link>
-    </header>
-        <main>
-            <Routes>
-                <Route exact path="/movie"> 
-                <MovieDetails/> 
-                </Route>
-                <Route  path="/"> 
-                <LandingPage/> 
-                </Route>
-             </Routes>
-        </main>
-        </Router>
-       
-    );
-};
+
+        export function App() {
+            return (
+              <Router>
+                <header>
+                  <Link to="/">
+                    <h1 className={styles.title}>Movies</h1>
+                  </Link>
+                </header>
+                <main>
+                  <Routes>
+                    <Route exact path="/movies/:movieId">
+                      <MovieDetails />
+                    </Route>
+                    <Route path="/">
+                      <LandingPage />
+                    </Route>
+                  </Routes>
+                </main>
+              </Router>
+            );
+          }
+          
 
 //Si yo selecciono F2 cambio el nombre de los componentes en todos sus espacios
 //React puede crear interfaces de usuario de forma reutilizable
--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,22 +1,25 @@
+import { useEffect, useState } from "react";
+import { get } from "../utils/httpClient";
 import { MovieCard } from "./MovieCard";
-import movies from "./movies.json";
 import styles from "./MoviesGrid.module.css";
 
 export function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    get("/discover/movie").then((data) => {
+      setMovies(data.results);
+    });
+  }, []);
   return (
- /* <ul>
-    {movies.map(function(movie) {
-       return <li>{movie.title}</li>
-    })}
-  </ul>
-  ); */
-  <ul className={styles.moviesGrid}> 
-    {movies.map((movie) => (
-  <MovieCard key={movie.id} movie={movie} />
-    ))}
-  </ul>
+    <ul className={styles.moviesGrid}>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </ul>
   );
-}  
+}
+
 //con funcion => 
 
  //traducimos cada objeto a un LI y luego lo mapeamos
