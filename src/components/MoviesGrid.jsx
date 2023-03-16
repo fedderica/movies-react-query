2c7e31e764618c96b6ed87b5e6f5ff8d2c6c00da
fedderica <fedderica.bartoli@gmail.com>
2023-03-16 11:08:29 -0300

    README.INFO-APP-FUNCIONANDOTRADATOSAPI+REMUEVEJSON

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,16 +1,34 @@
 import { useEffect, useState } from "react";
-import { get } from "../utils/httpClient";
+//import { get } from "../utils/httpClient";
 import { MovieCard } from "./MovieCard";
 import styles from "./MoviesGrid.module.css";
 
 export function MoviesGrid() {
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    get("/discover/movie").then((data) => {
+  useEffect(()  => {
+    fetch("https://api.themoviedb.org/3/discover/movie" , {
+      headers: {
+        Authorization: 
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmY3NzA0Nzc4MTA3ZDg2YTEwYzBhOGQ4Mzk4NmFjMSIsInN1YiI6IjY0MTMwYTc1ZTE4ZTNmMDdlOTU0YmNiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.359_yc09HWlkWBZJOfUZmzzd-UQM01Zkv3e4uu2A4Uk",
+        "Content-Type": "application/json;charset=utf-8" ,
+      },
+    })
+    .then((result) => result.json())
+    .then((data) => {
       setMovies(data.results);
     });
   }, []);
+    
+  
+
+ // useEffect(() => { //cuando mi componen se cargue, se ejecutara mi useEffect (efecto secundario)
+   // get("/discover/movie").then((data) => {
+     // setMovies(data.results);
+   // });
+ // }, []);
+ 
+
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
